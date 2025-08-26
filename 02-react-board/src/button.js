ccfca69fe136441bc559020e086e7eb29171f1d3
Jason Soh <padosjs@gmail.com>
2025-08-26 16:56:17 +0900

    Day 16 필수과제 완료

--- 02-react-board/src/button.js
+++ 02-react-board/src/button.js
@@ -1,8 +1,12 @@
+// button.js
 const 버튼 = (props) => {
     const buttonClass = `button ${props.className || 'white-button'}`;
     
     return (
-        <button className={buttonClass}>
+        <button
+            className={buttonClass}
+            onClick={props.onClick} // 이 부분을 추가하여 props로 받은 onClick 함수를 적용합니다.
+        >
             {props.icon && (
                 <span className="button-icon" style={{fill: props.iconColor}}>
                     <img src={props.icon} alt="icon" />
@@ -12,3 +16,5 @@ const 버튼 = (props) => {
         </button>
     );
 };
+
+export default 버튼
