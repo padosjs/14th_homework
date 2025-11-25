855484d843b171f281483dc2e104a5df7dfbe880
Jason Soh <padosjs@gmail.com>
2025-11-26 00:14:50 +0900

    Refactor access token store to use Zustand's persist middleware
    - Integrated Zustand's persist middleware to store access tokens in local storage.
    - Updated the access token state management to ensure persistence across sessions.

--- 08-new-travel/src/commons/stores/access-token-store.ts
+++ 08-new-travel/src/commons/stores/access-token-store.ts
@@ -1,17 +1,21 @@
 import { create } from 'zustand'
+import { persist } from 'zustand/middleware'
 
 interface AccessTokenState {
     accessToken: string;
     setAccessToken: (token: string) => void;
 }
 
-export const useAccessTokenStore = create<AccessTokenState>((set) => {
-
-    return {
-        accessToken: "",
-        setAccessToken: (로그인토큰) => {
-            set(() => ({ accessToken: 로그인토큰 }))
+export const useAccessTokenStore = create(
+    persist<AccessTokenState>(
+        (set) => ({
+            accessToken: "",
+            setAccessToken: (로그인토큰) => {
+                set(() => ({ accessToken: 로그인토큰 }))
+            }
+        }),
+        {
+            name: 'access-token-storage',
         }
-    }
-
-})
+    )
+)
